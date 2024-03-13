6f6ff00852c4fead6a495ca5f491451c8797104f
Andy <wuyuchen20120707@126.com>
2024-03-13 12:04:59 +0800

    The Main Function

--- cleaner.py
+++ cleaner.py
@@ -4,10 +4,7 @@ import easygui
 import time
 
 is_windows = None
-# Using os.name
 os_name = os.name
-
-# More detailed, using platform
 platform_system = platform.system()
 print(f"platform.system(): {platform_system}")
 
@@ -15,7 +12,6 @@ if os_name == 'posix':
     is_windows = False
 elif os_name == 'nt':
     is_windows = True
-# platform.system() gives a more readable format
 if platform_system == 'Linux':
     print("Windows Cleaner 1.1 built on Linux")
     print("Application Not Avaliable")
@@ -29,7 +25,7 @@ elif platform_system == 'Windows':
 run = False
 answer = easygui.buttonbox(msg="Are you going to start? Better close other applications.", title="Confirm", choices=("Yes", "Not Now"), default_choice = "Yes", image=None, images=None)
 if answer == "Yes":
-    run = True
+    pass
 elif answer == "No":
     exit(0)
 else:
@@ -41,5 +37,17 @@ if os.path.exists(r"C:\Windows\temp"):
 else:
     print("Target Disappeared. No need to clean.")
     exit(0)
+run = True
 
-    
+# The path to the folder
+folder_path = r'C:\Windows\temp'
+
+if run:
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if os.path.isfile(file_path):
+            try:
+                os.remove(file_path) 
+                print(f"Deleted: {filename}")
+            except Exception as e:
+                print(f"Error deleting {filename}: {e}")
